2939cf09a647e989823d82fcba7bdc4018496a3e
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-10-15 14:43:32 -0700

    stopping point - got the header section finished

---
+++ js/index.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var typedTitle = new Typed("#typed-title-loc", {
+  // Waits 1000ms after typing "First"
+  stringsElement: '#typed-title',
+  typeSpeed: 20,
+  showCursor: false,
+});
+
+var typedLang = new Typed("#typed-lang-loc", {
+  // Waits 1000ms after typing "First"
+  stringsElement: '#typed-languages',
+  typeSpeed: 30,
+});
+
+// var typed2 = new Typed("#typed-languages", {
+//   // Waits 1000ms after typing "First"
+//   strings: ["^2000C# and ASP.NET", "HTML, CSS, and JavaScript", "Java and Android"],
+//   typeSpeed: 20
+// });
+
+// var typed3 = new Typed('#typed', {
+//   stringsElement: '#typed-strings'
+// });
