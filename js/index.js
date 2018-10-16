f65a58ac54f1e9cff3dd8a1680626c03b3ff277c
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-10-15 17:43:33 -0700

    got rid of zombie code

--- js/index.js
+++ js/index.js
@@ -12,13 +12,3 @@ var typedLang = new Typed("#typed-lang-loc", {
   stringsElement: '#typed-languages',
   typeSpeed: 30,
 });
-
-// var typed2 = new Typed("#typed-languages", {
-//   // Waits 1000ms after typing "First"
-//   strings: ["^2000C# and ASP.NET", "HTML, CSS, and JavaScript", "Java and Android"],
-//   typeSpeed: 20
-// });
-
-// var typed3 = new Typed('#typed', {
-//   stringsElement: '#typed-strings'
-// });
