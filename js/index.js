2217c0679d2a2b3b88e037810117833455a487cb
Earl Jay Caoile <ecaoile@my.hpu.edu>
2018-10-18 01:10:23 -0700

    added target blank to the external links

--- js/index.js
+++ js/index.js
@@ -24,6 +24,10 @@ $('a.nav-link').on('click', function () {
   $('.navbar-toggle').click(); //bootstrap 3.x by Richard
   $('.navbar-toggler').click(); //bootstrap 4.x
 
+  // adds active class on nav link item when clicked
+  $('a.nav-link').removeClass('active');
+  $(this).closest('a.nav-link').addClass('active');
+
   // does a smooth scroll to the chosen section rather instantly moving to the section
   var href = $(this).attr('href');
   $('html,body').animate({
@@ -31,3 +35,33 @@ $('a.nav-link').on('click', function () {
   },
     'slow');
 });
+
+// Cache selectors
+// var topMenu = $("nav"),
+//   topMenuHeight = topMenu.outerHeight() + 15,
+//   // All list items
+//   menuItems = topMenu.find("a"),
+//   // Anchors corresponding to menu items
+//   scrollItems = menuItems.map(function () {
+//     var item = $($(this).attr("href"));
+//     if (item.length) { return item; }
+//   });
+
+// // Bind to scroll
+// $(window).scroll(function () {
+//   // Get container scroll position
+//   var fromTop = $(this).scrollTop() + topMenuHeight;
+
+//   // Get id of current scroll item
+//   var cur = scrollItems.map(function () {
+//     if ($(this).offset().top < fromTop)
+//       return this;
+//   });
+//   // Get the id of the current element
+//   cur = cur[cur.length - 1];
+//   var id = cur && cur.length ? cur[0].id : "";
+//   // Set/remove active class
+//   menuItems
+//     .parent().removeClass("active")
+//     .end().filter("[href='#" + id + "']").parent().addClass("active");
+// });​
